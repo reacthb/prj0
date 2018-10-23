099e27f6c1690c523d3c5b8ac0cd6d801ac2a429
Git-hb <hsbb@free.fr>
2018-10-23 09:27:15 +0200

    c1

---
+++ prj/public_html/js/vanilla.js
@@ -0,0 +1,11 @@
+/* 
+ * Version vanilla javascript
+ * Ici les instructions javascript ne seront pas transformées par Babel=>pas de JSX
+ * Pas d'import de React ni de ReactDOM
+ */
+
+var my_div = document.getElementById('root');
+my_div.innerHTML = "<h1>Hello, world!!! </h1>";
+
+
+
