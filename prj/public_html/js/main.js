64c18832d6d007711b2038da8952276890f6e57a
Git-hb <hsbb@free.fr>
2018-10-23 10:18:16 +0200

    c2

--- prj/public_html/js/main.js
+++ prj/public_html/js/main.js
@@ -1,10 +1,46 @@
+/*
+ * c1
+ *
 class App extends React.Component {
     render() {
-        return (
-                <h1>Hello world!!</h1>
-                );
+        return (<h1>Hello world!!</h1>);
     }
-}
-;
-
+};
 ReactDOM.render(<App />, document.getElementById('root'));
+*/
+
+var rootElt = document.getElementById('root');
+
+/*
+ * c2
+ */
+// Component Hello (Virtual DOM REACT vs DOM element)
+
+function Hello(x) {
+    return <h1>Hello, {x.what}</h1>;
+}
+var reactElt = <Hello what = "world!!"/>;
+var reactElt2 = Hello({what: "world!"});
+
+// Compare reactElt and reactElt2:
+console.log("reactElt: ", reactElt);
+console.log("reactElt2: ", reactElt2);
+
+
+ReactDOM.render(reactElt, rootElt);
+
+////////////////////////////////////////////////////////////////////////////////
+/* version sans JSX:
+ function Hello (x) {
+ const chd =     "Hello"  + x.what;
+ 
+ return React.createElement(
+ "h1",
+ null,
+ chd
+ );
+ }
+ 
+ ReactDOM.render(React.createElement(Hello, { what: "world!" }), rootElt);
+*/
+ ///////////////////////////////////////////////////////////////////////////////
--- prj/public_html/js/vanilla.js
+++ prj/public_html/js/vanilla.js
@@ -3,9 +3,11 @@
  * Ici les instructions javascript ne seront pas transformées par Babel=>pas de JSX
  * Pas d'import de React ni de ReactDOM
  */
-
+function Hello(x) {
+    return "<h1>Hello, " + x.what + "</h1>";
+}
 var my_div = document.getElementById('root');
-my_div.innerHTML = "<h1>Hello, world!!! </h1>";
+my_div.innerHTML = Hello({what: "world!"});
 
 
 
